087c7d73331745e25328c6cdd558d5c86b22dace
Sergey Zubtsovskiy <sergey.zubtsovskiy@gmail.com>
2017-01-04 21:38:27 +0100

    An attempt to use DnD API event

--- app/src/elm/App2.elm
+++ app/src/elm/App2.elm
@@ -5,7 +5,6 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (onInput, on, keyCode)
 import Html.Keyed as Keyed
 import Json.Decode as Json
-import Dict exposing (Dict)
 import Mouse exposing (Position)
 import String
 import Html.CssHelpers as CssHelpers
@@ -26,15 +25,15 @@ type alias ID =
   Int
 
 
-type alias ViewItem =
-  { title : String
-  , topLeft : Position
+type alias Item =
+  { dragged : Bool
+  , id : ID
+  , title : String
   }
 
 
 type alias Model =
-  { items : Dict ID ViewItem
-  , draggedItem : Maybe ( ID, ViewItem, Position, Position )
+  { items : List Item
   , newItemTitle : String
   }
 
@@ -42,9 +41,8 @@ type alias Model =
 type Msg
   = SetNewItemTitle String
   | KeyDown Int
-  | DragStart ID Position
-  | DragAt Position
-  | DragEnd Position
+  | DragStart ID
+  | DragOver ID
 
 
 init : ( Model, Cmd Msg )
@@ -52,36 +50,26 @@ init =
   let
     items =
       [ "First", "Second", "Third", "Fourth", "Fifth" ]
-        |> List.indexedMap (\i title -> ( i, ViewItem title { x = 0, y = i * itemBoxHeight + itemSpacing } ))
-        |> Dict.fromList
+        |> List.indexedMap (Item False)
 
     model =
       { items = items
-      , draggedItem = Nothing
       , newItemTitle = ""
       }
   in
-    model ! [ Cmd.none ]
+    model ! []
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  case model.draggedItem of
-    Just _ ->
-      Sub.batch
-        [ Mouse.moves DragAt
-        , Mouse.ups DragEnd
-        ]
-
-    Nothing ->
-      Sub.none
+  Sub.none
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update action model =
   case action of
     SetNewItemTitle value ->
-      { model | newItemTitle = value } ! [ Cmd.none ]
+      { model | newItemTitle = value } ! []
 
     KeyDown code ->
       case code of
@@ -89,108 +77,59 @@ update action model =
           if String.length model.newItemTitle > 0 then
             let
               itemCount =
-                Dict.size model.items
+                List.length model.items
 
               newItem =
-                ViewItem model.newItemTitle { x = 0, y = itemCount * itemBoxHeight + itemSpacing }
+                Item False itemCount model.newItemTitle
             in
-              { model | newItemTitle = "", items = Dict.insert itemCount newItem model.items } ! [ Cmd.none ]
+              { model | newItemTitle = "", items = model.items ++ [ newItem ] } ! []
           else
-            model ! [ Cmd.none ]
+            model ! []
 
         27 ->
-          { model | newItemTitle = "" } ! [ Cmd.none ]
+          { model | newItemTitle = "" } ! []
 
         _ ->
-          model ! [ Cmd.none ]
+          model ! []
 
-    DragStart id xy ->
-      case model.draggedItem of
-        Nothing ->
-          case Dict.get id model.items of
-            Just item ->
-              { model | items = Dict.remove id model.items, draggedItem = Just ( id, item, item.topLeft, xy ) } ! [ Cmd.none ]
+    DragStart id ->
+      let
+        markDragged item =
+          if item.id == id then
+            { item | dragged = True }
+          else
+            item
+      in
+        { model | items = List.map markDragged model.items } ! []
 
-            Nothing ->
-              model ! [ Cmd.none ]
+    DragOver id ->
+      let
+        target =
+          List.filter (\item -> item.id == id) model.items
+            |> List.head
 
-        Just _ ->
-          model ! [ Cmd.none ]
+        dragged =
+          List.filter (\item -> item.dragged) model.items
+            |> List.head
 
-    DragAt xy ->
-      case model.draggedItem of
-        Just ( id, item, orig, dragStarted ) ->
-          let
-            newY =
-              orig.y + xy.y - dragStarted.y
+        items =
+          case ( target, dragged ) of
+            ( Just target, Just dragged ) ->
+              let
+                f item acc =
+                  if item.id == target.id then
+                    acc ++ [ dragged ]
+                  else if item.id == dragged.id then
+                    acc ++ [ target ]
+                  else
+                    acc ++ [ item ]
+              in
+                List.foldl f [] model.items
 
-            minY =
-              itemSpacing
-
-            maxY =
-              (Dict.size model.items) * itemBoxHeight + itemSpacing
-
-            prevTopLeft =
-              item.topLeft
-
-            newTopLeft =
-              if newY > maxY then
-                Position orig.x maxY
-              else if newY >= minY then
-                Position orig.x newY
-              else
-                Position orig.x minY
-
-            newDraggedItem =
-              { item | topLeft = newTopLeft }
-
-            newItems =
-              Dict.map (over newTopLeft prevTopLeft) model.items
-          in
-            { model | items = newItems, draggedItem = Just ( id, newDraggedItem, orig, dragStarted ) } ! [ Cmd.none ]
-
-        Nothing ->
-          model ! [ Cmd.none ]
-
-    DragEnd xy ->
-      case model.draggedItem of
-        Just ( id, item, orig, _ ) ->
-          let
-            newY =
-              item.topLeft.y
-
-            adjustedY =
-              if newY > orig.y + itemBoxHeight // 2 || newY < orig.y - itemBoxHeight // 2 then
-                (toFloat newY)
-                  / (toFloat itemBoxHeight)
-                  |> round
-                  |> (*) itemBoxHeight
-                  |> (+) itemSpacing
-              else
-                orig.y
-
-            droppedItem =
-              { item | topLeft = Position item.topLeft.x adjustedY }
-          in
-            { model | items = Dict.insert id droppedItem model.items, draggedItem = Nothing } ! [ Cmd.none ]
-
-        Nothing ->
-          model ! [ Cmd.none ]
-
-
-over : Position -> Position -> ID -> ViewItem -> ViewItem
-over newTopLeft prevTopLeft _ item =
-  if newTopLeft.y < prevTopLeft.y then
-    -- moving upwards
-    if item.topLeft.y <= newTopLeft.y && newTopLeft.y <= item.topLeft.y + itemHeight // 2 then
-      { item | topLeft = Position item.topLeft.x (item.topLeft.y + itemBoxHeight) }
-    else
-      item
-  else if newTopLeft.y <= item.topLeft.y && newTopLeft.y + itemHeight >= item.topLeft.y + itemHeight // 2 then
-    -- moving downwards
-    { item | topLeft = Position item.topLeft.x (item.topLeft.y - itemBoxHeight) }
-  else
-    item
+            _ ->
+              model.items
+      in
+        { model | items = items } ! []
 
 
 
@@ -206,31 +145,11 @@ over newTopLeft prevTopLeft _ item =
   (,)
 
 
-itemHeight : Int
-itemHeight =
-  40
-
-
-itemSpacing : Int
-itemSpacing =
-  2
-
-
-itemBoxHeight : Int
-itemBoxHeight =
-  itemHeight + itemSpacing
-
-
 view : Model -> Html Msg
 view model =
   let
     items =
-      case model.draggedItem of
-        Just ( id, item, _, _ ) ->
-          (List.map (todo False) (Dict.toList model.items)) ++ [ todo True ( id, item ) ]
-
-        Nothing ->
-          (List.map (todo False) (Dict.toList model.items))
+      List.map todo model.items
   in
     div [ class [ Styles.Container ] ]
       [ div [ class [ Styles.ContentWrapper ] ]
@@ -259,26 +178,21 @@ groupTitle model =
   div [ class [ Styles.GroupTitle ] ] [ text "Monday" ]
 
 
-todo : Bool -> ( ID, ViewItem ) -> ( String, Html Msg )
-todo dragged ( id, item ) =
+todo : Item -> ( String, Html Msg )
+todo item =
   let
-    topLeft =
-      item.topLeft
-
     inlineStyles =
-      [ "position" => "absolute"
-      , "left" => px topLeft.x
-      , "top" => px topLeft.y
-      , "height" => px itemHeight
+      [ "height" => "40px"
+      , "margin-bottom" => "2px"
       ]
 
     classes =
       [ Styles.Item => True
-      , Styles.Dragged => dragged
+      , Styles.Dragged => item.dragged
       ]
   in
-    ( toString id
-    , div [ dataItemId id, onMouseDown, classList classes, style inlineStyles ]
+    ( toString item.id
+    , div [ dataItemId item.id, classList classes, style inlineStyles, draggable "true", onDragStart item.id, onDragOver item.id ]
         [ span [] [ text item.title ]
         ]
     )
@@ -289,9 +203,14 @@ onKeyDown tagger =
   on "keydown" (Json.map tagger keyCode)
 
 
-onMouseDown : Attribute Msg
-onMouseDown =
-  on "mousedown" (Json.map2 DragStart dataItemIdDecoder positionDecoder)
+onDragStart : ID -> Attribute Msg
+onDragStart id =
+  on "dragstart" (Json.succeed (DragStart id))
+
+
+onDragOver : ID -> Attribute Msg
+onDragOver id =
+  on "dragover" (Json.succeed (DragOver id))
 
 
 dataItemId : Int -> Attribute Msg
