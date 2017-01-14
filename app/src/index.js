96ef60e55a72670a095e8434ca2385b1523c3319
Sergey Zubtsovskiy <sergey.zubtsovskiy@gmail.com>
2017-01-14 11:42:20 +0100

    Upgrade elm-webpack-loader and elm-hot-loader (fixes hot reloading issue)

--- app/src/index.js
+++ app/src/index.js
@@ -1,13 +1,3 @@
-import firebase from 'firebase';
 import {App} from './elm/App';
 
 App.embed(document.getElementById('content'));
-
-var config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DB_URL,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
-};
-firebase.initializeApp(config);
